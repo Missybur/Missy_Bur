4d0caa0aff419a4a83a63a334c5433c28fa075fb
Missy Bur <missybur@gmail.com>
2015-10-13 15:47:02 -0700

    Add updated solution to challenge 4

--- Coding_House_Prework/4.js
+++ Coding_House_Prework/4.js
@@ -1,22 +1,37 @@
+// var numWords;
+// var characters;
+
+// function numWords(str){
+//   var words = str.split("");
+//   var numWords = words.length;
+//   return numWords;
+//   var characters = str.length;
+//   return characters;
+// }
+
+// var output = '';
+// for (var property in object) {
+//   output += property + ': ' + object[property]+'; ';
+// }
+
+// console.log(numWords("Hi, this is a test string"));
+
+// function wordCount(str, sep) {
+
+// return str.split(' ').length;
+// };
+
+// console.log(wordCount("Hi, this is a test string"));
+
 function numberText(str) {
-    var words = str.split(" ");
-    console.log(words);
-    var numWords = words.length; //1. Number of words in the text
-    console.log(numWords);
-    var chars = str.length; //2. Number of characters in the text
-    console.log(chars);
-    var spaces = numWords - 1; //3. Number of spaces in a text
-    console.log(spaces);
+    var wordsSplit = str.split(" ");
+    var words = wordsSplit.length;
+    var chars = str.length;
+    var spaces = words - 1;
+    var justWords = chars - spaces;
+    var avgLength = justWords/words;
 
-    sum = 0
-    for ( var i = 0; i < words.length; i++) {
-        sum += words[i].length;
-    }
-    var averageWord = sum/words.length;
-    console.log(averageWord); //4. Average word length
-
-
-    console.log(numWords + " words, " + chars + " characters, " + averageWord + " average word length & "  + spaces + " spaces");
+    var result = {"words": words, "chars": chars, "avgLength": avgLength, "spaces": spaces};
+    return result;
 }
 
-numberText("Count me in" );
