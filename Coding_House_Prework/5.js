97aa17e89ff5a57e33fb8b76b6f4944369abcf85
Missy Bur <missybur@gmail.com>
2015-10-13 14:56:27 -0700

    Add answer to challenge 5

--- Coding_House_Prework/5.js
+++ Coding_House_Prework/5.js
@@ -1,7 +1,11 @@
-var str = "Life is 10% what happens to you, and 90% of how you REACT to it";
-
-var swapCase =
-    str.replace("Life is 10% what happens to you, and 90% of how you REACT to it", "lIFE IS 10% WHAT HAPPENS TO YOU, AND 90% OF HOW YOU react TO IT");
-
-console.log(str);
-console.log(swapCase);
+var swapCase = function(str){
+  var newLetters = "";
+  for (var i = 0; i < str.length; i++){
+    if(str[i] === str[i].toLowerCase()){
+      newLetters += str[i].toUpperCase();
+    } else {
+        newLetters += str[i].toLowerCase();
+    }
+  }
+  return newLetters;
+}
