5fac197f42b3a32aa54078decf6286808de4eab1
Missy Bur <missybur@gmail.com>
2015-10-17 17:05:40 -0700

    Add jquery to main.js page for challenge 20

--- Coding_House_Prework/20/main.js
+++ Coding_House_Prework/20/main.js
@@ -1,3 +1,6 @@
+// $(document).ready(function(){
+
+
 for (var i=0; i< 64; i++){
     document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'black' : 'white';
 }
@@ -55,4 +58,4 @@ function rgb_to_hex(red, green, blue)
       $(this).html(rainbowtext);
     });
   };
-})( jQuery );
+});
