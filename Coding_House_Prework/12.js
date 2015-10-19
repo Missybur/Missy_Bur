18f5231ca09f79c63a7867ec93abb3cbfa12179b
Missy Bur <missybur@gmail.com>
2015-10-18 20:30:36 -0700

    Add updated answer to challenge 12

--- Coding_House_Prework/12.js
+++ Coding_House_Prework/12.js
@@ -1,22 +1,24 @@
-function dateDifference(month, day, year) {
+function dateDiffChecker(year, month, day){
 
   var date1 = new Date();
   var date2 = new Date(month + '/' + day + '/' + year);
-  var date1Month = date1.getMonth() + 1;
-  var date2Month = date2.getMonth() + 1;
-  var date1Day = date1.getDate();
-  var date2Day = date2.getDate();
-  var date1Year = date1.getFullYear();
-  var date2Year = date2.getFullYear();
-  var monthDiff = Math.abs(date1Month - date2Month);
-  var dayDiff = Math.abs(date1Day - date2Day);
-  var yearDiff = Math.abs(date1Year - date2Year);
-  var dateMonth = new Date();
-  var month = dateMonth.getMonth() + 1
+  var month1 = date1.getMonth() + 1;
+  var month2 = date2.getMonth() + 1;
+  var day1= date1.getDate();
+  var day2 = date2.getDate();
+  var year1 = date1.getFullYear();
+  var year2 = date2.getFullYear();
 
-  var finalMonthDayYearDiff = yearDiff + " year(s), " +monthDiff + " month(s), " + dayDiff + " day(s) ";
+  var yearDiff = Math.abs(year1 - year2);
+  var monthDiff = Math.abs(month1 - month2);
+  var dayDiff = Math.abs(day1 - day2);
 
-  return finalMonthDayYearDiff;
+
+  var finalDateDifference =  yearDiff + " year(s) " + monthDiff + " month(s) " + dayDiff + " day(s) "
+
+  return finalDateDifference
 }
 
-console.log(dateDifference(2010, 3, 20));
+
+
+console.log(dateDiffChecker(2010, 2, 17));
