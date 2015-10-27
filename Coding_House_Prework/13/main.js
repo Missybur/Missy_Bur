c22e3875453c3e8f304dfec328cfdd75a28c2f09
Missy Bur <missybur@gmail.com>
2015-10-26 22:05:25 -0700

    Add answer to challenge 13

--- Coding_House_Prework/13/main.js
+++ Coding_House_Prework/13/main.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 
-  function invertColor(hexTripletColor) {
-      var color = hexTripletColor;
-      color = color.substring(1);           // remove #
-      color = parseInt(color, 16);          // convert to integer
-      color = 0xFFFFFF ^ color;             // invert three bytes
-      color = color.toString(16);           // convert to hex
-      color = ("000000" + color).slice(-6); // pad with leading zeros
-      color = "#" + color;                  // prepend #
-      return color;
+  function funColor(){
+
+    function invertColor(hexTripletColor) {
+        var color = hexTripletColor;
+        color = color.substring(1);           // remove #
+        color = parseInt(color, 16);          // convert to integer
+        color = 0xFFFFFF ^ color;             // invert three bytes
+        color = color.toString(16);           // convert to hex
+        color = ("000000" + color).slice(-6); // pad with leading zeros
+        color = "#" + color;                  // prepend #
+        return color;
+    }
+
+    $("div").each(function(index, element){
+      var colorRand = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      var newColor = invertColor(colorRand);
+      $(this).css("background-color", colorRand);
+      $(this).css("color", newColor);
+    });
+
   }
 
-  $(".invert-color").each(function(){
-    $(this).css("background-color", invertColor());
-  });
+    $("#button").on ("click", funColor);
+
 });
 
 
