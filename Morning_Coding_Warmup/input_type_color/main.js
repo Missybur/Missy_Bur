792dba5156bef956c6de5d0fdbe0db7ff2dc03a1
Missy Bur <missybur@gmail.com>
2015-10-30 23:04:42 -0700

    Ad changes to hw

--- Morning_Coding_Warmup/input_type_color/main.js
+++ Morning_Coding_Warmup/input_type_color/main.js
@@ -6,7 +6,46 @@ function init() {
   console.log('ready!');
   $('#makeBox').on('click', makeBox);
   $('#randomBox').on('click', randomBox);
+  $('.container').on('click', '.box', boxClicked);
+  $('.container').click(makeBox);
 }
+
+function moveBox(){
+  var $this = $(this);
+
+  if (!$('.selected').length || $this.children('.selected').length){
+
+    return;
+}
+
+  var $selected = $('.selected').detach();
+  $this.append($selected.detach().removeClass('selected'));
+}
+
+function boxClicked(event){
+  event.stopPropagation();
+  var $target = $(event.target);
+
+  var isSelected = $target.hasClass('selected');
+
+  if (isSelected){
+    $target.removeClass('selected');
+  } else {
+    $('.selected').removeClass('selected');
+    $target.addClass('selected');
+  }
+}
+
+  function moveSelected(){
+
+    console.log('moveSelected');
+    if ($(this).find(".selected").length > 0){
+
+    }else{detached = $('.selected').detach().removeClass('selected');
+        $(this).append(detached)};
+  }
+
+
 
 function makeBox() {
   var color = $('#color').val();
@@ -15,6 +54,17 @@ function makeBox() {
 }
 
 function randomBox() {
+  var color = $('#color').val();
+  var $box = makeColoredBox(color);
+  $('#boxes').append($box);
+}
+
+function makeColoredBox(color){
+  var $box = $('<div>');
+  $box.addClass('box');
+  $box.css('background-color', color);
+  return $box;
+}
   // var colorValue = Math.floor(parseInt('ffffff', 16) * Math.random()).toString(16);
   // while(colorValue.length < 6){
   //   colorValue = '0' + colorValue;
@@ -22,9 +72,9 @@ function randomBox() {
   // var color = '#' + colorValue;
   // var color = "rgb(255,255,255)";
 
-  var $box = makeColoredBox(randomRGBa());
-  $('#boxes').append($box);
-}
+//   var $box = makeColoredBox(randomRGBa());
+//   $('#boxes').append($box);
+// }
 
 function randomRGB(){
   var r = random255();
@@ -46,11 +96,4 @@ function random255(){
 }
 
 
-
-function makeColoredBox(color){
-  var $box = $('<div>');
-  $box.addClass('box');
-  $box.css('background-color', color);
-  return $box;
-}
 
